277f63980943e78ab1210aa076fd77cb749566f9
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-06-10 15:49:17 -0400

    functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Clicker from "./Clicker";
+import "./App.css";
+import background from "./background.jpg";
 
 function App() {
+  const [start, setStart] = useState(false);
+  const [replay, setReplay] = useState(false);
+  const [number, setNumber] = useState(0);
+
+  const endGame = (points) => {
+    setStart(false);
+    setReplay(true);
+    setNumber(points);
+  };
+
+  const startGame = () => {
+    setStart(true);
+    setReplay(false);
+  };
+
+  const divStyle = {
+    height: "100vh",
+    backgroundImage: !start ? `url(${background})` : "none",
+  };
+
   return (
-    <div>
-      <h1> App</h1>
+    <div style={divStyle} className="ui container center aligned">
+      {!start && (
+        <div className="ui brown segment">
+          {replay ? (
+            <h1 className="ui header">
+              Good Job! You scored {number} points. Play again?
+            </h1>
+          ) : (
+            <h1 className="ui header">Welcome to the Cookie Clicker Game</h1>
+          )}
+
+          <p>
+            Try to click on the cookie as many times as you can. You only have
+            10 seconds. Good luck!
+          </p>
+          <button className="ui brown basic button" onClick={startGame}>
+            Start
+          </button>
+        </div>
+      )}
+      {start && <Clicker endGame={endGame} />}
     </div>
   );
 }
---
+++ src/Clicker.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import cookie from "./cookie.png";
+
+function Clicker(props) {
+  const [left, setLeft] = useState("50%");
+  const [top, setTop] = useState("50%");
+  const [points, setPoints] = useState(0);
+  const [time, setTime] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let randomLeftPosition = `${
+        Math.floor(Math.random() * (90 - 0 + 1)) + 0
+      }%`;
+      let randomTopPosition = `${
+        Math.floor(Math.random() * (90 - 0 + 1)) + 0
+      }%`;
+
+      setLeft(randomLeftPosition);
+      setTop(randomTopPosition);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (time === 0) {
+      props.endGame(points);
+    }
+
+    const interval = setInterval(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [time]);
+
+  const addPoints = () => {
+    setPoints(points + 1);
+  };
+
+  return (
+    <div className="ui container">
+      <img
+        style={{
+          position: "absolute",
+          left,
+          top,
+        }}
+        onClick={addPoints}
+        src={cookie}
+      />
+
+      <h3 className="ui grey right floated header">{time} seconds left</h3>
+
+      <h1 className="ui center aligned header">
+        <span style={{ color: "#654321" }}> {points} </span> points
+      </h1>
+    </div>
+  );
+}
+
+export default Clicker;
